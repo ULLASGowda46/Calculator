2cc51eeb40e8255906219ff481f3b90cd6040689
ULLASK18 <141169279+ULLASK18@users.noreply.github.com>
2024-01-26 23:04:35 +0530

    calcApp

---
+++ app.js
@@ -0,0 +1,21 @@
+function appendToScreen(value) {
+  document.getElementById('screen').value += value;
+}
+
+function clearScreen() {
+  document.getElementById('screen').value = '';
+}
+
+function deletePreNum() {
+  document.getElementById('screen').value = '';
+}
+
+
+function calculate() {
+  try {
+    const result = eval(document.getElementById('screen').value);
+    document.getElementById('screen').value = result;
+  } catch (error) {
+    document.getElementById('screen').value = 'Error';
+  }
+}
